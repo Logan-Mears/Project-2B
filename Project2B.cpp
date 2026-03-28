c169f9fd5f1069937cdb4f4fb046ae567c2936aa
Logan Mears <logan.mears@ufl.edu>
2026-03-28 13:26:57 -0400

    Created person struct w/ constructor and displays

--- Project2B.cpp
+++ Project2B.cpp
@@ -4,3 +4,43 @@
 using namespace std;
 
 
+// PERSON STRUCT ===============================================================================================================================================================
+//All rows from CSV files will be processed as a PERSON struct, which will be stored in a vector of PERSON structs. 
+// Top line of CSV: Index,User Id,First Name,Last Name,Sex,Email,Phone,Date of birth,Job Title
+struct PERSON {
+    int index;
+    int userId;
+    string first;
+    string last;
+    string sex;
+    string email;
+    string phone;
+    string dob;
+    string occupation;
+
+    // Constructor for PERSON struct
+    PERSON(int idx, int uid, string f, string l, string s, string e, string p, string d, string o) {
+        index = idx;
+        userId = uid;
+        first = f;
+        last = l;
+        sex = s;
+        email = e;
+        phone = p;
+        dob = d;
+        occupation = o;
+    }
+
+    // Display (if needed)
+    void print() {
+        cout << "Index: " << index << endl;
+        cout << "User ID: " << userId << endl;
+        cout << "First Name: " << first << endl;
+        cout << "Last Name: " << last << endl;
+        cout << "Sex: " << sex << endl;
+        cout << "Email: " << email << endl; 
+        cout << "Phone: " << phone << endl;
+        cout << "Date of Birth: " << dob << endl;
+        cout << "Occupation: " << occupation << endl;
+    }
+};
